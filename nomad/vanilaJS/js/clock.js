ab91f16e8b84e0505218c1818ddee03007b8fcc7
RamSSi <001229aram@gmail.com>
2022-05-04 02:03:29 +0900

    Nomad Coder : Vanila JS - making TO DO LIST

--- nomad/vanilaJS/js/clock.js
+++ nomad/vanilaJS/js/clock.js
@@ -2,7 +2,12 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
   const date = new Date();
-  clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  const milliseconds = String(date.getMilliseconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
-getClock(); // 웹 사이트가 새로고침 될 때마다 새로운 Date 객체를 생성(새로운 시간)
-setInterval(getClock, 1000);
+getClock(); // 웹 사이트가 새로고침 될 때마다 새로운 Date 객체를 생성(새로고침을 누른 시간)
+setInterval(getClock, 70);
+var a = "1";
