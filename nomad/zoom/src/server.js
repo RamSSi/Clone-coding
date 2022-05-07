ce6340329e0e3ac283a9df4989d2b9661908d41c
RamSSi <001229aram@gmail.com>
2022-05-07 18:41:55 +0900

    Nomad Coder : Zoom Clone Coding - Setting again

--- nomad/zoom/src/server.js
+++ nomad/zoom/src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import { WebSocketServer } from "ws";
 import express from "express";
 
 const app = express();
@@ -5,7 +7,18 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.render("home"));
 
-const handleListen = () => console.log(`Linstening on http://localhost:3000`);
-app.listen(3000, handleListen);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+function handleConnection(socket) {
+  console.log(socket);
+}
+wss.on("connection", handleConnection);
+
+
+server.listen(3000, handleListen);
