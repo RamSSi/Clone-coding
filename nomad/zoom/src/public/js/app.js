14a853f8ea92601b745293c9fb062b803d824d37
RamSSi <001229aram@gmail.com>
2022-05-08 21:33:21 +0900

    Nomad Coder : Zoom Clone Coding - Chatting List

--- nomad/zoom/src/public/js/app.js
+++ nomad/zoom/src/public/js/app.js
@@ -1,1 +1,44 @@
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = {type, payload};
+  return JSON.stringify(msg);
+}
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✔");
+});
+
+socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server 😢");
+});
+
+// setTimeout(() => {
+//   socket.send("hello from the browser");
+// }, 1000);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(makeMessage("new_chat", input.value));
+  input.value ="";
+}
+
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+  input.value ="";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- nomad/zoom/src/server.js
+++ nomad/zoom/src/server.js
@@ -1,6 +1,7 @@
 import http from "http";
 import { WebSocketServer } from "ws";
 import express from "express";
+import { parse } from "path";
 
 const app = express();
 
@@ -15,10 +16,51 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocketServer({ server });
 
-function handleConnection(socket) {
-  console.log(socket);
-}
-wss.on("connection", handleConnection);
+const sockets = [];
+let anon = 0;
+
+
+wss.on("connection", (socket) => {  // Browser와 연결되면 실행
+  sockets.push(socket); // 서로 다른 유저들끼리 연결해주기 위해 소켓 정보 저장
+  anon = (anon + 1) % 10;
+  if (anon === 0) anon += 1;
+  socket["nickname"] = `Anon${anon}`;
+
+  sockets.forEach((connectedSocket) => {
+    if (connectedSocket === socket) {
+      return
+    }
+    if (connectedSocket["nickname"] === socket["nickname"]) {
+      socket["nickname"] = `Anon${++anon}`;
+    }
+  });
+  console.log("Connected to Browser ✔"); // Browser와 연결되었음을 출력
+    // if (msg.toString("utf-8") === "Disconnected") {
+    //   anon--;
+    //   console.log(`${socket[nickname]} Disconnected`);
+    //   return
+    // }
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_chat": 
+        sockets.forEach((connectedSocket) => connectedSocket.send(`${socket.nickname}: ${message.payload}`));
+        break;
+      case "nickname": 
+        socket["nickname"] = message.payload;
+        anon--;
+        break;
+    // sockets.forEach((connectedSocket) => connectedSocket.send(message.toString("utf-8")));
+    }
+  });
+  socket.addEventListener("close", () => {
+    sockets.forEach((connectedSocket, index) => {
+      if (connectedSocket === socket) {
+        sockets.splice(index, 1);
+      }
+    })
+  })
+});
 
 
 server.listen(3000, handleListen);
